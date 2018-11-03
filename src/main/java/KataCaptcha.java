5db17c848dd60717efead2d3fa420d09757a7d6c
dujthep <dujthep@odd.works>
2018-11-03 14:26:17 +0700

    Calculate leftOperandPlusRight & Question One

--- src/main/java/KataCaptcha.java
+++ src/main/java/KataCaptcha.java
@@ -35,7 +35,25 @@ public class KataCaptcha {
         return stringNumber[rightOperand - 1];
     }
 
-    public String concatString() {
-        return String.join(" ", this.getLeftOperandFirstPattern(),this.getOperator() ,this.getRightOperandFirstPattern());
+    public String concatStringFirstPattern() {
+        return String.join(" ", this.getLeftOperandFirstPattern(), this.getOperator(), this.getRightOperandFirstPattern());
+    }
+
+    public String concatStringSecondPattern() {
+        return String.join(" ", this.getLeftOperandSecondPattern(), this.getOperator(), this.getRightOperandSecondPattern());
+    }
+
+    public int calculateResult() {
+        if (operator == 1) return this.leftOperand + this.rightOperand;
+        else if(operator == 2) return this.leftOperand * this.rightOperand;
+        else return this.leftOperand - this.rightOperand;
+    }
+
+    public String concatResult() {
+        if(this.pattern == 1) {
+            return this.concatStringFirstPattern() + " = "+ this.calculateResult();
+        } else {
+            return this.concatStringSecondPattern() + " = "+ this.calculateResult();
+        }
     }
 }
--- src/test/java/KataCapthcaTest.java
+++ src/test/java/KataCapthcaTest.java
@@ -100,15 +100,43 @@ public class KataCapthcaTest {
 
     @Test
     public void firstPatternShouldBeOneplus1() throws Exception {
-        KataCaptcha captcha = new KataCaptcha(1, 1,1,1);
-        String actual = captcha.concatString();
+        KataCaptcha captcha = new KataCaptcha(1, 1, 1, 1);
+        String actual = captcha.concatStringFirstPattern();
         Assert.assertEquals("One + 1", actual);
     }
 
     @Test
     public void firstPatternShouldBeNineMinus9() throws Exception {
-        KataCaptcha captcha = new KataCaptcha(1, 9,3,9);
-        String actual = captcha.concatString();
+        KataCaptcha captcha = new KataCaptcha(1, 9, 3, 9);
+        String actual = captcha.concatStringFirstPattern();
         Assert.assertEquals("Nine - 9", actual);
+    }
+
+    @Test
+    public void secondPatternShould1MultipleOne() throws Exception {
+        KataCaptcha captcha = new KataCaptcha(2, 1, 2, 1);
+        String actual = captcha.concatStringSecondPattern();
+        Assert.assertEquals("1 * One", actual);
+    }
+
+    @Test
+    public void secondPaaternShouldBe5PlusFive() throws Exception {
+        KataCaptcha captcha = new KataCaptcha(2, 5, 1, 5);
+        String actual = captcha.concatStringSecondPattern();
+        Assert.assertEquals("5 + Five", actual);
+    }
+
+    @Test
+    public void leftOperandPlusRightOperandShouldBe2() throws Exception {
+        KataCaptcha captcha = new KataCaptcha(1,1,1,1);
+        int actual = captcha.calculateResult();
+        Assert.assertEquals(2,actual);
+    }
+
+    @Test
+    public void QuestionOne() throws Exception {
+        KataCaptcha captcha = new KataCaptcha(1,1,1,1);
+        String actual = captcha.concatResult();
+        Assert.assertEquals("One + 1 = 2", actual);
     }
 }
